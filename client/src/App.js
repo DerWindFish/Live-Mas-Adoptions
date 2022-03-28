f3fc7c6b99c0fd603401552a63b31498dd970711
Mark Ryan <markrryan99@gmail.com>
2022-03-28 11:48:40 -0700

    nav impemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import './App.css'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div>
+      <header>
+        <Nav />
+      </header>
+    
       I'm just a div and life is a nightmare
     </div>
   );
