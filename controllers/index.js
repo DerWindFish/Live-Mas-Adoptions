b28950770bdf2b61b23f5cbf57d5657b49962a85
Mark Ryan <markrryan99@gmail.com>
2022-03-31 13:34:53 -0700

    fixed issue with put function crashing server

--- controllers/index.js
+++ controllers/index.js
@@ -60,20 +60,29 @@ const getVolunteerById = async (req, res) => {
 const updateVolunteerInfo = async (req, res) => {
     try {
         const { id } = req.params;
-        await Volunteer.findByIdAndUpdate(id, req.body, { new: true }, (err, volunteer) => {
-            if (err) {
-                res.status(500).send(err);
-            }
-            if (!volunteer) {
-                res.status(500).send('Volunteer not found!');
-            }
-            return res.status(200).json(volunteer);
-        })
+        const volunteer = await Volunteer.findByIdAndUpdate(id, req.body, { new: true },)
+        return res.status(200).json(volunteer)
     } catch (error) {
         return res.status(500).send(error.message);
     }
 }
 
+// const updateVolunteerInfo = async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         await Volunteer.findByIdAndUpdate(id, req.body, { new: true }, (err, volunteer) => {
+//             if (err) {
+//                 res.status(500).send(err);
+//             }
+//             if (!volunteer) {
+//                 res.status(500).send('Volunteer not found!');
+//             }
+//             return res.status(200).json(volunteer);
+//         })
+//     } catch (error) {
+//         return res.status(500).send(error.message);
+//     }
+// }
 const deleteVolunteer = async (req, res) => {
     try {
         const { id } = req.params;
